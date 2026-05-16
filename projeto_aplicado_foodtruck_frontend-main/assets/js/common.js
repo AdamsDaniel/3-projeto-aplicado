cf2daf666dc1954c87a1478fbed448f0b75675d4
AdamsDaniel <dnkndaniel1@gmail.com>
2026-05-15 21:28:59 -0300

    feat: add frontend service to docker-compose, configure Pydantic settings to ignore extra fields, and update frontend API base URL

--- projeto_aplicado_foodtruck_frontend-main/assets/js/common.js
+++ projeto_aplicado_foodtruck_frontend-main/assets/js/common.js
@@ -3,8 +3,8 @@
 
 // Definição da URL base do seu backend.
 // IMPORTANTE: Ajuste esta porta se o seu backend rodar em outra porta (ex: 5000)
-const API_BASE_URL = 'http://foodtruck.docker.localhost/';
-//const API_BASE_URL = 'http://localhost:8000/'; // Se estiver rodando localmente
+//const API_BASE_URL = 'http://foodtruck.docker.localhost/';
+const API_BASE_URL = 'http://localhost:8000'; // Se estiver rodando localmente
 //const API_BASE_URL = 'http://89.117.33.177/';
 
 // Anexa a variável ao objeto global 'window'
